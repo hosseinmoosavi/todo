28b6f9e2dd8abbd8f70fecd2a6f26b075826e7a9
Your Name <hossein.moosavi78@gmail.com>
2022-12-17 01:01:52 +0330

    first commit

---
+++ src/components/frames/frame.jsx
@@ -0,0 +1,125 @@
+import "./frame.css";
+import Tasks from "../tasks/tasks";
+import { useState, useRef } from "react";
+
+function Frame() {
+  const [input, setInput] = useState();
+  const [taskarray, setTaskarray] = useState([]);
+  const inputRef = useRef();
+  const [suborsave,setsuborsave]=useState("submit") 
+  const [editid,setEditid]=useState() 
+  const [newValue,setNewValue]=useState() 
+ 
+  
+  
+  //-----------------Delete----------------
+
+  function deleteHandler(id) {
+    setTaskarray(taskarray.filter((task) => task.id != id));
+
+  }
+//-----------------input----------------
+  function inputHandler(event) {
+    setInput(() => event.target.value);
+    setNewValue(()=>event.target.value)
+  }
+//--------------------submit----------------
+function submitHandler() {
+  if(input==""){ 
+    alert("please write a task")
+  }else{
+ if  (suborsave==="submit"){
+  if (taskarray.some((obj) => obj.tasktilte === input)) {
+    alert("task exists");
+    return;
+  }
+
+  
+  const sum = taskarray.length + 1;
+  setTaskarray([
+      ...taskarray,
+      (taskarray.tasktilte = { id: sum, tasktilte: input }),
+    ]);
+    setInput("");
+    inputRef.current.focus();
+  }
+  else{
+    setsuborsave("submit")
+    saveHandler()
+    setInput("");
+    inputRef.current.focus();
+  }
+  }
+}
+  function handleKeyUp(event) {
+    if (event.keyCode === 13) {
+      submitHandler();
+    }
+  }
+  
+  //--------------------edit----------------
+  
+  function editHandler(id){
+   if (input===""){ 
+    const editinput=taskarray[id-1].tasktilte
+    setInput(editinput)
+    setEditid(id)
+    inputRef.current.focus();
+    
+    setsuborsave("save");
+  }else{
+    alert("finish editing")
+  }
+}
+
+  function saveHandler(){  
+        const newTaskArray = taskarray.map((item) => {
+      if(item.id == editid){
+        return {
+          id: editid,
+          tasktilte: newValue
+        }
+      }
+      else{
+        return item
+      }
+    })
+   setTaskarray(newTaskArray) 
+  
+  }
+
+
+  return (
+    <div className="frame">
+      <div className="tasksection">
+        {taskarray.map((taskarray) => {
+          return (
+            <Tasks
+              Taskdelete={taskarray}
+              Tasktilte={taskarray.tasktilte}
+              Id={taskarray.id}
+              hiu_delete={deleteHandler}
+              hiu_edit={editHandler}
+              hiu_save={saveHandler}
+            />
+          );
+        })}
+      </div>
+      <div className="addsection">
+        <input
+          id="taskInput"
+          onKeyUp={handleKeyUp}
+          value={input}
+          onChange={inputHandler}
+          className="addtask"
+          ref={inputRef}
+          placeholder="Add your tasks"
+        ></input>
+        <button type="submit" onClick={submitHandler} className="sbutton">
+          {suborsave}
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Frame;
---
+++ src/components/tasks/tasks.jsx
@@ -0,0 +1,45 @@
+import "./tasks.css"
+import { useState } from "react";
+
+
+
+
+function Tasks({ Tasktilte, Id,hiu_delete,hiu_edit,hiu_save}){
+    const [classid,setClassid]=useState("is")
+
+
+
+function editHandler(){
+
+    hiu_edit(Id)
+    hiu_save(Id)
+}
+
+
+    function checkedHandler(event){
+        classid==="done"?
+        setClassid("is"):
+        setClassid("done")
+
+    }
+
+    function deleteHandler(){
+      hiu_delete(Id)
+    }
+
+    return(
+
+        <div className="fulltask">
+            <div className="taskcontainer">
+            <input onChange={checkedHandler} type='checkbox' ></input>
+            <h2>{Id}-</h2>
+            <h2 className={classid}> {Tasktilte}</h2>
+            </div>
+            <div>
+            <button onClick={deleteHandler} className="deletebtn">Delete</button>
+            <button className="editbtn" onClick={editHandler}>Edit</button>
+            </div>
+        </div>
+    )
+}
+export default Tasks;
